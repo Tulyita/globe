0c059280ac80f9d0f88d64bfc492106143a6a342
Jacob Grahn <contact@jiggmin.com>
2014-01-23 21:54:21 -0500

    removed last mockery dependency, using sinon instead

--- spec/routes/bansGetSpec.js
+++ spec/routes/bansGetSpec.js
@@ -1,66 +1,52 @@
 'use strict';
 
-///////////////////////////////////////////////////////////////
-// mocks
-///////////////////////////////////////////////////////////////
-var db = {
-	abc: {
-		_id: 'abc',
-		bans: [
-			{name: 'ban1'},
-			{name: 'ban2'}
-		]
-	}
-};
-
-var MockUser = {
-	findById: function(_id, fields, callback) {
-		if(_id === 'errorTime') {
-			return callback('There was a horrible error');
-		}
-		return callback(null, db[_id]);
-	}
-};
-
-
-
-////////////////////////////////////////////////////////////////
-// dependencies
-////////////////////////////////////////////////////////////////
-var mockery = require('mockery');
-mockery.registerAllowables(['../../server/routes/bansGet']);
-mockery.registerMock('../models/user', MockUser);
-mockery.enable();
-
+var sinon = require('sinon');
+var User = require('../../server/models/user');
 var bansGet = require('../../server/routes/bansGet');
 
-mockery.disable();
-mockery.deregisterAll();
 
-
-////////////////////////////////////////////////////////////////
-// tests
-////////////////////////////////////////////////////////////////
 describe('bansGet', function() {
 
-	it('should return what mongoose finds', function(done) {
-		var req = {
-			body: {userId: 'abc'}
-		};
-		bansGet(req, {apiOut: function(err, resp) {
-			expect(err).toBeFalsy();
-			expect(resp.bans).toEqual([{name: 'ban1'}, {name: 'ban2'}]);
-			done();
-		}});
-	});
 
-	it('should return an error if mongoose returns an error', function(done) {
-		var req = {
-			body: {userId: 'errorTime'}
-		};
-		bansGet(req, {apiOut: function(err) {
-			expect(err).toBeTruthy();
-			done();
-		}});
+	//////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////
+
+	describe('get', function() {
+
+		beforeEach(function() {
+			sinon.stub(User, 'findById');
+		});
+
+		afterEach(function() {
+			User.findById.restore();
+		});
+
+		it('should return what mongoose finds', function(done) {
+			User.findById
+				.withArgs('abc')
+				.yields(null, {_id: 'abc',bans: [{name: 'ban1'}, {name: 'ban2'}]});
+			var req = {
+				body: {userId: 'abc'}
+			};
+			bansGet(req, {apiOut: function(err, resp) {
+				expect(err).toBeFalsy();
+				expect(resp.bans).toEqual([{name: 'ban1'}, {name: 'ban2'}]);
+				done();
+			}});
+		});
+
+		it('should return an error if mongoose returns an error', function(done) {
+			User.findById
+				.withArgs('errorTime')
+				.yields('There was a horrible error');
+			var req = {
+				body: {userId: 'errorTime'}
+			};
+			bansGet(req, {apiOut: function(err) {
+				expect(err).toBeTruthy();
+				done();
+			}});
+		});
 	});
 });
