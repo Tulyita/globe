2d0f830f653cf82dd2edbd7a90621294af1112cb
Jacob Grahn <contact@jiggmin.com>
2014-05-04 00:29:40 -0400

    accept requests from futurism-http and futurism-multi

--- server/fns/serverKeys.js
+++ server/fns/serverKeys.js
@@ -3,7 +3,7 @@
 
 module.exports = function(appName, key) {
 
-	if(appName === 'futurism' && key === process.env.FUTURISM_KEY && process.env.FUTURISM_KEY) {
+	if(appName.indexOf('futurism') !== -1 && key === process.env.FUTURISM_KEY && process.env.FUTURISM_KEY) {
 		return true;
 	}
 	else if(appName === 'pr4' && key === process.env.PR4_KEY && process.env.PR4_KEY) {
