87b755f8defec5446b63d317541d09d7d91ea1b1
Jacob Grahn <contact@jiggmin.com>
2014-05-15 22:38:10 -0400

    accept gp from servers that know the right password

--- server/guildRoutes.js
+++ server/guildRoutes.js
@@ -5,8 +5,6 @@ module.exports = function (globe) {
 
     // endpoints
     var banner = require('./routes/guilds/banner');
-    var guildGp = require('./routes/guilds/guildGp');
-    var guilds = require('./routes/guilds/guilds');
     var kick = require('./routes/guilds/kick');
     var kicks = require('./routes/guilds/kicks');
     var member = require('./routes/guilds/member');
@@ -14,23 +12,18 @@ module.exports = function (globe) {
     var mod = require('./routes/guilds/mod');
     var mods = require('./routes/guilds/mods');
     var settings = require('./routes/guilds/settings');
-    var userGp = require('./routes/guilds/userGp');
 
 
     // middleware
     var checkUser = require('./middleware/checkUser');
-    var checkServer = require('./middleware/checkServer');
-    var rateLimit = require('./middleware/rateLimit');
     var continueSession = require('./middleware/continueSession');
     var loadMyself = require('./middleware/loadMyself');
     var loadUser = require('./middleware/loadUser');
 
-    var applicantExists = require('./routes/guilds/middleware/applicantExists');
     var guildModExists = require('./routes/guilds/middleware/guildModExists');
     var memberExists = require('./routes/guilds/middleware/memberExists');
     var isGuildMod = require('./routes/guilds/middleware/isGuildMod');
     var isOwner = require('./routes/guilds/middleware/isOwner');
-    var isOwnerOrSelf = require('./routes/guilds/middleware/isOwnerOrSelf');
     var isSelf = require('./routes/guilds/middleware/isSelf');
     var loadGuild = require('./routes/guilds/middleware/loadGuild');
 
@@ -38,17 +31,13 @@ module.exports = function (globe) {
     // routes
     require('./routes/guilds/invites').init(globe);
     require('./routes/guilds/guilds').init(globe);
+    require('./routes/guilds/guildGp').init(globe);
     require('./routes/guilds/applicants').init(globe);
-
-    globe.post('/guilds/:guildId/gp', loadGuild, checkServer, guildGp.post);
-    globe.get('/guilds/:guildId/gp', loadGuild, guildGp.get);
 
     globe.get('/guilds/:guildId/members', loadGuild, members.get);
     globe.get('/guilds/:guildId/members/:userId', loadGuild, memberExists, member.get);
     globe.put('/guilds/:guildId/members/:userId', loadGuild, continueSession, isSelf, checkUser, member.put);
     globe.del('/guilds/:guildId/members/:userId', loadGuild, continueSession, isSelf, checkUser, member.del);
-    globe.post('/guilds/:guildId/members/:userId/gp', loadGuild, memberExists, checkServer, userGp.post);
-    globe.get('/guilds/:guildId/members/:userId/gp', loadGuild, memberExists, userGp.get);
 
     globe.get('/guilds/:guildId/kicks', loadGuild, kicks.get);
     globe.get('/guilds/:guildId/kicks/:userId', loadGuild, kick.get);
--- server/routes/guilds/guildGp.js
+++ server/routes/guilds/guildGp.js
@@ -1,23 +1,47 @@
-'use strict';
+(function() {
+    
+    'use strict';
 
-var _ = require('lodash');
-
-
-module.exports = {
-
-
-	post: function(req, res) {
-		req.guild.gp += req.query.inc;
-		req.guild.save(function(err) {
-			if(err) {
-				return res.apiOut(err);
-			}
-			return res.apiOut(null, _.pick(req.guild, 'gp', 'gpDay', 'gpWeek', 'gpLife'));
-		});
-	},
+    var _ = require('lodash');
+    var loadGuild = require('./middleware/loadGuild');
+    var memberExists = require('./middleware/memberExists');
+    var checkServer = require('../../middleware/checkServer');
 
 
-	get: function(req, res) {
-		return res.apiOut(null, _.pick(req.guild, 'gp', 'gpDay', 'gpWeek', 'gpLife'));
-	}
-};
+    var self = {
+
+
+        init: function(app) {
+            app.post('/guilds/:guildId/members/:userId/gp', checkServer, loadGuild, memberExists, self.post);
+            app.get('/guilds/:guildId/members/:userId/gp', loadGuild, memberExists, self.get);
+            app.get('/guilds/:guildId/gp', loadGuild, self.getGuildGp);
+        },
+
+
+        post: function(req, res) {
+            req.guild.incGp(req.params.userId, Number(req.body.inc), function(err) {
+                if(err) {
+                    return res.apiOut(err);
+                }
+
+                var user = req.guild.getMember(req.params.userId);
+                return res.apiOut(null, _.pick(user, 'gpDay', 'gpWeek', 'gpLife'));
+            });
+        },
+        
+        
+        get: function(req, res) {
+            var user = req.guild.getMember(req.params.userId);
+            return res.apiOut(null, _.pick(user, 'gpDay', 'gpWeek', 'gpLife'));
+        },
+        
+        
+        getGuildGp: function(req, res) {
+            return res.apiOut(null, _.pick(req.guild, 'gp', 'gpDay', 'gpWeek', 'gpLife'));
+        }
+    };
+    
+    
+    module.exports = self;
+    
+}());
